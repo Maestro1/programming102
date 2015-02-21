ff0205fab6408a0beafe4c533a639939f040cbe5
Maestro1 <danielkarama13@gmail.com>
2015-02-21 14:37:31 +0300

    Sato Intro class

--- examples/01-hello.py
+++ examples/01-hello.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-print("Hello", "World!")
+print("Jambo world!")
+def name():
+	nm=input('Enter your name\n')
+	if nm =='kevin':
+		print('Wassup Fat Tony')
+	elif nm=='maestro':print('Heey Admin cup of coffee?')	
+	else:print('Hey '+nm+' hakuna matata')
+name()
